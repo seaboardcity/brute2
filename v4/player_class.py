cc031ffbe75ac355f81d79fbc8dbae347bb88b8a
Paul Wayner <pwayner@idirect.net>
2012-11-20 15:36:58 -0500

    foo

--- v4/player_class.py
+++ v4/player_class.py
@@ -9,14 +9,23 @@ import tv
 
 ## This class contains the dynamic information for a player 
 #
-# status, can have 'stunned' or 'wounded' appended
+# status, can have 'stunned' or 'wounded' appended as needed
+# init can have 'faceoff', 'have', 'dont have', ['faceoff red']
+# cp is a list with the cp's player has. This includes the virtual cp's
+# Cards is the card list the player has. 
 
-class pl:
+# temporarily create lists with [ [<card number>, <short term value>],
+#                                 [<card number>, <short term value>, etc
+
+class player:
 
     def __init__(self, name, abilities):
         'constructor. Names the char, sets status information'
         self.char = name
         self.status = ''
-
+        self.init = 'faceoff'
+        self.cp = ''
+        self.cards = ''
+        self.abilities = ''
 
         
